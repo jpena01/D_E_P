d7328f27061a6ba1362b34c09a1064306dee3d9e
Joshua Peña <Joshuap12321@gmail.com>
2025-04-30 23:04:37 -0500

    Added ability to automatically create data directory

--- storage/json_store.py
+++ storage/json_store.py
@@ -1,9 +1,13 @@
 import json
 import os
 
+DATA_DIR = "data"
 DATA_PATH = "data/tasks.json"
 
 def load_tasks():
+    if not os.path.exists(DATA_DIR):
+        os.makedirs(DATA_DIR)
+
     if not os.path.exists(DATA_PATH):
         return []
     with open(DATA_PATH, "r") as f:
